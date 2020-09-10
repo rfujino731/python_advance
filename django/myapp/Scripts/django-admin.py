b11d20fe75dab26514c10168596ffdb02aed411d
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-09-10 21:23:53 +0900

    django modified settings.py models.py

--- django/myapp/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\user\desktop\work\python_advance\django\myapp\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
