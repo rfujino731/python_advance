59853833cc2faae355d38d36153f339c3a284ea8
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-09-12 18:16:42 +0900

    django write queryset in views.py

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .model import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #今回はデプロイしないので、publicに関する記述はいらないが、見本通りに記述する。
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'post': posts})
